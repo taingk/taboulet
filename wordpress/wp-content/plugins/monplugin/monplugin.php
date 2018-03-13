72edfb87aa5471526af4f669e406a7d65d2ba78a
Amandine Ripoll <amandine-ripoll@outlook.fr>
2018-03-13 16:49:55 +0100

    hurry

--- wordpress/wp-content/plugins/monplugin/monplugin.php
+++ wordpress/wp-content/plugins/monplugin/monplugin.php
@@ -23,7 +23,9 @@ class Mon_Plugin
   public function __construct()
   {
     include_once plugin_dir_path(__FILE__).'newsletter.php';
-    register_activation_hook(__FILE__, array('Mon Plugin', 'install'));
+    register_activation_hook(__FILE__, array('Mon_Plugin', 'install'));
+    register_uninstall_hook(__FILE__, array('Mon_Plugin', 'uninstall'));
+    add_action('wp_loaded', array($this, 'save_email'));
   }
 
   public static function install()
@@ -31,6 +33,27 @@ class Mon_Plugin
     global $wpdb;
     $wpdb->query("CREATE TABLE IF NOT EXISTS {$wpdb->prefix}ma_newsletter_list (id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) NOT NULL);");
   }
+
+  public static function uninstall()
+  {
+    global $wpdb;
+    $wpdb->query("DROP DATABASE IF EXISTS {$wpdb->prefix}ma_newsletter_list;");
+  }
+
+  public function save_email()
+  {
+    if(isset($_POST['ma_newsletter_email']) & !empty($_POST['ma_newsletter_email']))
+    {
+      global $wpdb;
+      $email = $_POST['ma_newsletter_email'];
+      $row = $wpdb->get_row("SELECT * FROM {$wpdb->prefix}ma_newsletter_list WHERE email = '$email'");
+      if (is_null($row))
+      {
+        $wpdb->insert("{$wpdb->prefix}ma_newsletter_list", array('email' => $email));
+      }
+    }
+  }
+
 }
 
 new Mon_Plugin();
