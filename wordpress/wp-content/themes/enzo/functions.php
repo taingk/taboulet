d35fbe450bdf4a5434b0040088776b420aef6bff
Amandine Ripoll <amandine-ripoll@outlook.fr>
2018-03-14 10:40:35 +0100

    Recette

--- wordpress/wp-content/themes/enzo/functions.php
+++ wordpress/wp-content/themes/enzo/functions.php
@@ -84,6 +84,57 @@ add_action('wp_enqueue_scripts', 'wpdocs_theme_name_scripts');
 
 
 
+/*
+* On utilise une fonction pour créer notre custom post type 'Séries TV'
+*/
+
+function wpm_custom_post_type() {
+
+	// On rentre les différentes dénominations de notre custom post type qui seront affichées dans l'administration
+	$labels = array(
+		// Le nom au pluriel
+		'name'                => _x( 'Recette', 'Post Type General Name'),
+		// Le nom au singulier
+		'singular_name'       => _x( 'Recette', 'Post Type Singular Name'),
+		// Le libellé affiché dans le menu
+		'menu_name'           => __( 'Recette'),
+		// Les différents libellés de l'administration
+		'all_items'           => __( 'Toutes les recettes'),
+		'view_item'           => __( 'Voir les recettes'),
+		'add_new_item'        => __( 'Ajouter une nouvelle recette'),
+		'add_new'             => __( 'Ajouter'),
+		'edit_item'           => __( 'Editer la recette'),
+		'update_item'         => __( 'Modifier la recette'),
+		'search_items'        => __( 'Rechercher une recette'),
+		'not_found'           => __( 'Non trouvée'),
+		'not_found_in_trash'  => __( 'Non trouvée dans la corbeille'),
+	);
+
+	// On peut définir ici d'autres options pour notre custom post type
+
+	$args = array(
+		'label'               => __( 'Recette'),
+		'description'         => __( 'Toutes nos meilleures recettes'),
+		'labels'              => $labels,
+		// On définit les options disponibles dans l'éditeur de notre custom post type ( un titre, un auteur...)
+		'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+		/*
+		* Différentes options supplémentaires
+		*/
+		'hierarchical'        => false,
+		'public'              => true,
+		'has_archive'         => true,
+		'rewrite'			  => array( 'slug' => 'recette'),
+
+	);
+
+	// On enregistre notre custom post type qu'on nomme ici "serietv" et ses arguments
+	register_post_type( 'recette', $args );
+
+}
+
+add_action( 'init', 'wpm_custom_post_type', 0 );
+
 
 
 ?>
